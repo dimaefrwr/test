76a2f6e6182013abf00ec161646a892c7c26afe1
lalat <156008036+dimaefrwr@users.noreply.github.com>
2025-03-15 23:40:39 +0100

    Add frontend files

---
+++ frontend/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@constants': './src/constants',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
+  };
